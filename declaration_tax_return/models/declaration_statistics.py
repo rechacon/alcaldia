49a43887c208dcecbd2a77c8f9e0a276f57b7e74
Franyer Hidalgo <fhidalgo.dev@gmail.com>
2022-05-03 20:45:51 -0400

    [IMP]: Vistas de estadisticas

--- declaration_tax_return/models/declaration_statistics.py
+++ declaration_tax_return/models/declaration_statistics.py
@@ -8,14 +8,15 @@ class AccountDeclarationStatistics(models.Model):
     partner_id = fields.Many2one('res.partner', 'Contribuyente')
     total_tax = fields.Integer('Total de Declaraciones')
     total_payment = fields.Integer('Total de Plantillas de Pago')
-    template_statistics_ids = fields.One2many('account.template.type.statistics','statistics_id','Estadística por Plantilla')
-    type_statistics_ids = fields.One2many('account.declaration.tax.type.statistics','statistics_id','Estadística por Impuesto')
-    
+    template_statistics_ids = fields.One2many('account.template.type.statistics', 'statistics_id', 'Estadística por Plantilla')
+    type_statistics_ids = fields.One2many('account.declaration.tax.type.statistics', 'statistics_id', 'Estadística por Impuesto')
+
+
 class AccountTemplateTypeStatistics(models.Model):
     _name = 'account.template.type.statistics'
     _description = 'Estadisticas de tipo de planilla'
-    
-    name_id = fields.Many2one('account.template.type', 'Tipo')
+
+    name = fields.Many2one('account.template.type', 'Tipo')
     total = fields.Float('Total')
     statistics_id = fields.Many2one('account.declaration.statistics', 'Estadística')
 
@@ -23,8 +24,7 @@ class AccountTemplateTypeStatistics(models.Model):
 class AccountTaxTypeStatistics(models.Model):
     _name = 'account.declaration.tax.type.statistics'
     _description = 'Estadisticas de tipo de impuesto'
-    
-    name_id = fields.Many2one('account.template.type', 'Tipo')
+
+    name = fields.Many2one('account.template.type', 'Tipo')
     total = fields.Float('Total')
     statistics_id = fields.Many2one('account.declaration.statistics', 'Estadística')
-
