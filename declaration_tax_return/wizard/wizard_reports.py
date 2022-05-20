b7b35dcb86fec9ed6522b0d5f5bf7759157c2c11
Franyer Hidalgo <fhidalgo.dev@gmail.com>
2022-05-20 11:11:24 -0400

    IMP: Ejemplo de graficos

--- declaration_tax_return/wizard/wizard_reports.py
+++ declaration_tax_return/wizard/wizard_reports.py
@@ -1,4 +1,9 @@
 from odoo import fields, models
+from matplotlib import pyplot as plt
+
+import base64
+import io
+import numpy as np
 
 
 class WizardReports(models.TransientModel):
@@ -9,6 +14,31 @@ class WizardReports(models.TransientModel):
     date_end = fields.Date('Fecha Fin', required=True)
     tax_ids = fields.Many2many('account.tax.return', string="Declaraciones")
     # payment_ids = fields.Many2many('account.tax.return', string="Planillas de pagos")
+
+    def get_graphic(self):
+        ypoints = np.array([3, 8, 1, 10])
+        plt.plot(ypoints, linestyle='solid')
+
+        ypoints = np.array([6, 2, 3, 12])
+        plt.plot(ypoints, linestyle='solid', color="red")
+
+        plt.grid(True, axis='y')
+        figure = plt.gca()
+        x_axis = figure.axes.get_xaxis()
+        x_axis.set_visible(False)
+
+        plt.legend(loc="upper left",
+                   mode='expand',
+                   ncol=1,
+                   )
+
+        plt.grid(True, axis='y')
+
+        img = io.BytesIO()
+        plt.savefig(img, dpi=100, format='png')
+        plt.close()
+
+        return base64.b64encode(img.getvalue())
 
     def create_report_municipal_comparison_graphic(self):
         """Crear reporte del comparativo municipal"""
