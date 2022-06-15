5a52b3e6cdba8d8efba5e0de6ca2a701b4321810
JeancastroVE <jeancastro.developerve@gmail.com>
2022-06-15 11:51:44 -0400

    FIX

--- l10n_ve_payroll_public/models/res_company.py
+++ l10n_ve_payroll_public/models/res_company.py
@@ -7,7 +7,7 @@ class ResCompany(models.Model):
     salary_min = fields.Float(string='Salario Mínimo', digits=(12, 2))
     employer_contribution_risk = fields.Integer(string='% Riesgo Aporte Patronal')
     employee_contribution_risk = fields.Integer(string='% Riesgo Aporte Empleado')
-    daily_food_bonus = fields.Float(string='Bono de Alimentación', digits=(12, 2))
+    bonus_food = fields.Float(string='Bono de Alimentación', digits=(12, 2))
     incentive_antiquity_ids = fields.One2many('hr.incentives.antiquity', 'company_id', string='Prima de Antigüedad')
     incentive_professionalization_ids = fields.One2many('hr.incentives.professionalization', 'company_id', string='Prima de Profesionalización')
     incentive_child = fields.Float(string='Prima por Hijo', digits=(12, 2))
