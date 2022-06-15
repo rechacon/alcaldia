8f92f2433aecfc84f2ab24454fa7abe0b73d431f
JeancastroVE <jeancastro.developerve@gmail.com>
2022-06-15 11:29:33 -0400

    DELETE: Desuso de Campos en la Estructura y Enviado A Company

--- l10n_ve_payroll_public/models/hr_payroll_structure_type.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-
-class HrPayrollStructureType(models.Model):
-    _inherit = 'hr.payroll.structure.type'
-
-    days_utilities_annual = fields.Integer(string='Días de Utilidades Anuales')
-    days_vacation_annual = fields.Integer(string='Días de Vacacionales Anuales')
-    days_bonus_vacations = fields.Integer(string='Dias de Bonos Vacacionales')
